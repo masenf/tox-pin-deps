d58c3ff191c20113f537a76410047507c77f20ff
Masen Furer <m_github@0x26.net>
2022-11-25 23:55:32 -0800

    test_plugin: align logic with test_plugin4.test_install
    use the same conditionals and types of assertions
    at the same part in the function.

--- tests/unit/test_plugin.py
+++ tests/unit/test_plugin.py
@@ -150,27 +150,29 @@ def test_tox_testenv_install_deps(
     if ignore_pins:
         venv.get_resolved_dependencies.assert_not_called()
         assert venv.envconfig.deps == deps
-    else:
+    elif pip_compile:
         venv.get_resolved_dependencies.assert_called_once()
-        if pip_compile and deps:
-            if env_requirements is None:
-                env_requirements = tox_pin_deps.requirements_file(
-                    toxinidir=venv.envconfig.config.toxinidir,
-                    envname=venv.envconfig.envname,
-                )
-            assert venv.envconfig.deps[0].name == f"-r{env_requirements}"
-            assert len(venv.envconfig.deps) == 1
-            assert len(venv._pcall.mock_calls) == 2
-            assert venv._pcall.mock_calls[0][1] == (["pip", "install", "pip-tools"],)
-            cmd = venv._pcall.mock_calls[1][1][0]
-            assert cmd[0] == "pip-compile"
-            # not mocking tempfile at this time
-            # assert cmd[1] == tf.name
-            start_idx = cmd.index("--output-file")
-            assert cmd[start_idx:] == ["--output-file", str(env_requirements)]
-        else:
-            assert venv.envconfig.deps == deps
-            venv._pcall.assert_not_called()
+        if env_requirements is None:
+            env_requirements = tox_pin_deps.requirements_file(
+                toxinidir=venv.envconfig.config.toxinidir,
+                envname=venv.envconfig.envname,
+            )
+        assert venv.envconfig.deps[0].name == f"-r{env_requirements}"
+        assert len(venv.envconfig.deps) == 1
+        assert len(venv._pcall.mock_calls) == 2
+        assert venv._pcall.mock_calls[0][1] == (["pip", "install", "pip-tools"],)
+        cmd = venv._pcall.mock_calls[1][1][0]
+        assert cmd[0] == "pip-compile"
+        # not mocking tempfile at this time
+        # assert cmd[1] == tf.name
+        start_idx = cmd.index("--output-file")
+        assert cmd[start_idx:] == ["--output-file", str(env_requirements)]
+    elif env_requirements:
+        assert venv.envconfig.deps[0].name == f"-r{env_requirements}"
+        venv._pcall.assert_not_called()
+    else:
+        assert venv.envconfig.deps == deps
+        venv._pcall.assert_not_called()
 
 
 def test_tox_testenv_install_deps_will_install(
